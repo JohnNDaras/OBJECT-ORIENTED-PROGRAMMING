25df48abfecbb97f432e2bafd56d976d1c4f8fbd
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-01-09 20:18:13 +0200

    Add files via upload

---
+++ Schoolscape_1/Part_1/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MAX_ARRAY_SIZE 1000
+#define locationClassroom 1
+#define locationYard 2
+#define locationHallway 3
+#define locationStairs 4
+#define locationOut 5
+#define locationCorridor 6
+
+#endif
+
