54ffc5eea30b2f1aa76d8e84c4b430fb3c87072d
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-01-09 21:51:47 +0200

    Add files via upload

---
+++ Schoolscape_2/Part_2/Configuration.java
@@ -0,0 +1,13 @@
+package myschool;
+
+
+public class Configuration {
+
+    public static final int MAX_ARRAY_SIZE = 1000;
+    public static final int locationClassroom = 1;
+    public static final int locationYard = 2;
+    public static final int locationHallway = 3;
+    public static final int locationStairs = 4;
+    public static final int locationOut = 5;
+    public static final int locationCorridor = 6;
+}
