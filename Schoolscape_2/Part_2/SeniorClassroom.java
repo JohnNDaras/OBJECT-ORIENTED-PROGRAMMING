e516457f8608b256a8b4b51f7af1ac0a47a4cbdd
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-01-09 21:54:47 +0200

    Add files via upload

---
+++ Schoolscape_2/Part_2/SeniorClassroom.java
@@ -0,0 +1,15 @@
+
+package myschool;
+
+
+public class SeniorClassroom extends Classroom {
+	
+	public SeniorClassroom(){
+            super();
+            classroomtype = "Senior Classroom";
+	}
+	public SeniorClassroom(int c){
+            super(c);
+            classroomtype = "Senior Classroom";
+	}
+}
