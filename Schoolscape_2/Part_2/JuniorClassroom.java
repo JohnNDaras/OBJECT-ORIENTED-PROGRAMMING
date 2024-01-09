251e5bd8fa8f1d88d4461713efaed6b470add32e
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-01-09 21:53:18 +0200

    Add files via upload

---
+++ Schoolscape_2/Part_2/JuniorClassroom.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myschool;
+
+/**
+ *
+ * @author Admin
+ */
+public class JuniorClassroom extends Classroom {
+
+    public JuniorClassroom() {
+        super();
+        classroomtype = "Junior Classroom";
+    }
+
+    JuniorClassroom(int c) {
+        super(c);
+        classroomtype = "Junior Classroom";
+    }
+};
