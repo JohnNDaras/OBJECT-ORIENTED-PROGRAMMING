351e63cf1d25e4b6d6a26951c3e7ac716acb6e22
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-01-09 22:03:55 +0200

    Add files via upload

---
+++ RealmCraft/Exoskeleton.cpp
@@ -0,0 +1,8 @@
+#include "Exoskeleton.h"
+
+Exoskeleton::Exoskeleton(){
+	Monster();
+}
+Exoskeleton::Exoskeleton(string n, int l, double hp, double da, double de, double av):Monster(n,l,hp,da,de,av){
+	
+}
